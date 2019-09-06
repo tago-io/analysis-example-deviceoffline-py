1ab2e3742756e2771725a5c46024e53b3f3aa041
Felipe Lima <felipe@ferreiradelima.com>
2019-09-06 19:15:47 -0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -8,14 +8,14 @@ from tago import Tago
 
 # The function myAnalysis will run when you execute your analysis
 def myAnalysis(context, scope):
-  # This will log "Hello World" at the Tago Analysis console
+  # This will log "Hello World" at the TagoIO Analysis console
   context.log("Hello World")
 
-  #  This will log the context to the Tago Analysis console
-  context.log('Context:', context)
+  #  This will log the environment to the TagoIO Analysis console
+ context.log('Environment:', context.environment)
 
-  #  This will log the scope to the Tago Analysis console
+  #  This will log the scope to the TagoIO Analysis console
   context.log('my scope:', scope)
 
-
+# The analysis token in only necessary to run the analysis outside TagoIO
 Tago('MY-ANALYSIS-TOKEN-HERE').analysis.init(myAnalysis)
