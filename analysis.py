cd4dec9534737d0053b9844cc1f57116e9901c11
Felipe Lima <felipe@ferreiradelima.com>
2019-09-13 14:00:36 -0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,7 +4,7 @@
 # Learn how to send messages to the console located on the TagoIO analysis screen.
 # You can use this principle to show any information during and after development.
 
-from tago import Tago
+from tago import Analysis
 
 # The function myAnalysis will run when you execute your analysis
 def myAnalysis(context, scope):
@@ -18,4 +18,4 @@ def myAnalysis(context, scope):
   context.log('my scope:', scope)
 
 # The analysis token in only necessary to run the analysis outside TagoIO
-Tago('MY-ANALYSIS-TOKEN-HERE').analysis.init(myAnalysis)
+Analysis('MY-ANALYSIS-TOKEN-HERE').init(myAnalysis)
