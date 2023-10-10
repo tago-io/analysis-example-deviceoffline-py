322db07b7949b05cc876791ec07db552e5824cc7
mateuscardosodeveloper <mateus.informatica44@gmail.com>
2023-10-10 11:49:07 -0300

    Updated code to new version of sdk

--- analysis.py
+++ analysis.py
@@ -13,7 +13,7 @@ check_in_time: Minutes between the last input of the device before sending the n
 tag_key: Device tag Key to filter the devices.
 tag_value: Device tag Value to filter the devices.
 email_list: Email list comma separated.
-sms_list: Phone number list comma separated. The phone number must include the country code
+sms_list: Phone number list comma separated. The phone number must include the country code. Example: +5511999999999
 
 Steps to generate an account_token:
 1 - Enter the following link: https://admin.tago.io/account/
@@ -24,7 +24,7 @@ Steps to generate an account_token:
 """
 from datetime import datetime
 
-from tagoio_sdk import Account, Analysis, Services
+from tagoio_sdk import Resources, Analysis, Services
 from tagoio_sdk.modules.Utils.envToJson import envToJson
 
 
@@ -32,14 +32,13 @@ def my_analysis(context, scope: list = None):
     # Transform all Environment Variable to JSON.
     env = envToJson(context.environment)
 
-    if not env.get("account_token"):
-        return print("You must setup an account_token in the Environment Variables.")
-    elif not env.get("check_in_time"):
+    if not env.get("check_in_time"):
         return print("You must setup a check_in_time in the Environment Variables.")
-    elif not env.get("tag_key"):
-        return print("You must setup a tag_key in the Environment Variables.")
-    elif not env.get("tag_value"):
-        return print("You must setup a tag_value in the Environment Variables.")
+    # You can remove the comments bellow and line 59 to use the Tag Filter.
+    # elif not env.get("tag_key"):
+    #     return print("You must setup a tag_key in the Environment Variables.")
+    # elif not env.get("tag_value"):
+    #     return print("You must setup a tag_value in the Environment Variables.")
     elif not env.get("email_list") and not env.get("sms_list"):
         return print(
             "You must setup an email_list or a sms_list in the Environment Variables."
@@ -49,16 +48,13 @@ def my_analysis(context, scope: list = None):
     if check_in_time == 0:
         return print("The check_in_time must be a number.")
 
-    account = Account(params={"token": env["account_token"]})
+    resources = Resources()
 
-    # You can remove the comments on line 51 and 57 to use the Tag Filter.
-    # filter = {'tags': [{'key': env['tag_key'], 'value': env['tag_value']}]}
-
-    devices = account.devices.listDevice(queryObj={
+    devices = resources.devices.listDevice(queryObj={
         "page": 1,
         "amount": 1000,
         "fields": ["id", "name", "last_input"],
-        # "filter": filter,
+        # "filter": {'tags': [{'key': env['tag_key'], 'value': env['tag_value']}]},
     })
 
     if not devices:
@@ -81,8 +77,7 @@ def my_analysis(context, scope: list = None):
         return print("All devices are okay.")
 
     print("Sending notifications")
-    email_service = Services(params={"token": context.token}).email
-    sms_service = Services(params={"token": context.token}).sms
+    services = Services()
 
     message = f"Hi!\nYou're receiving this alert because the following devices didn't send data in the last {check_in_time} minutes.\n\nDevices:"
     message += "\n".join(alert_devices)
@@ -91,22 +86,28 @@ def my_analysis(context, scope: list = None):
         # Remove space in the string
         emails = env["email_list"].replace(" ", "")
 
-        email_service.send(email={
-            "to": emails,
-            "subject": "Device Offline Alert",
-            "message": message,
-        })
+        try:
+            services.email.send(email={
+                "to": emails,
+                "subject": "Device Offline Alert",
+                "message": message,
+            })
+        except Exception as error:
+            print(error)
 
     if env.get("sms_list"):
         # Remove space in the string and convert to an Array.
         smsNumbers = env["sms_list"].replace(" ", "").split(",")
 
         for phone in smsNumbers:
-            sms_service.send(sms={
-                "to": phone,
-                "message": message,
-            })
+            try:
+                services.sms.send(sms={
+                    "to": phone,
+                    "message": message,
+                })
+            except Exception as error:
+                print(error)
 
 
 # The analysis token in only necessary to run the analysis outside TagoIO
-Analysis(params={"token": "MY-ANALYSIS-TOKEN-HERE"}).init(my_analysis)
+Analysis.use(my_analysis, params={"token": "MY-ANALYSIS-TOKEN-HERE"})
